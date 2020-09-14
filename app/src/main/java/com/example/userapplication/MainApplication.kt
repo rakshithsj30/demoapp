4f9fa3ab74414aa0d601cb83f9f7a377cd36d3ce
Rakshith <rakshith.s@smarterhomes.com>
2020-09-14 08:56:05 +0530

    Application Class

---
+++ app/src/main/java/com/example/userapplication/MainApplication.kt
@@ -0,0 +1,18 @@
+package com.example.userapplication
+
+import android.app.Application
+import com.example.userapplication.di.mainModule
+import org.koin.android.ext.android.startKoin
+
+class MainApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // starting Koin DI
+        startKoin(this,
+                listOf(mainModule),
+                loadPropertiesFromFile = true)
+
+    }
+}
