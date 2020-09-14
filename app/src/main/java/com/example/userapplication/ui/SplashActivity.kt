89343a4127cab459488d7342245aa3669c8d68f6
Rakshith <rakshith.s@smarterhomes.com>
2020-09-14 08:49:36 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/userapplication/ui/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.userapplication.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.userapplication.R
+
+private const val SPLASH_TIME_OUT:Long = 5000 // 5 sec
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,ImageListActivity::class.java))
+            // close this activity
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
